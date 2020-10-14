edfb3c213fd612da828c1fc3fde84492b985722d
Peter Pan <petarzivkovic85@gmail.com>
2020-10-13 21:06:15 -0700

    able to view departments, employes and roles

---
+++ schema.sql
@@ -0,0 +1,51 @@
+DROP DATABASE IF EXISTS employee_DB;
+
+CREATE DATABASE employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR (30) NOT NULL,
+    PRIMARY KEY (id)
+
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL (10,4) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES  department (id)
+);
+
+CREATE TABLE employee (
+    id NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
+);
+
+INSERT INTO department (name) VALUES("legal"),
+INSERT INTO department (name) VALUES("managment"),
+    
+INSERT INTO role (title, salary, department_id) VALUES ("manager", 87.234,1), 
+INSERT INTO role (title, salary, department_id) VALUES ("employee", 69.348,2),
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("John", "Doe",1,NULL),
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Daphne","Moon",2,1),
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Emily","Smith",3,2)
+
+    
+
+    
+
+
+
+
+
--- server.js
+++ server.js
@@ -9,14 +9,124 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password:"password",
-    database: "employee_trackerDB",
+    database: "employee_DB",
 });
 
 // connect to the mysql server and sql database
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id" + connection.threadId);
-    start();
 });
+
+const askQ = function() {
+    inquirer
+      .prompt({
+        type: "list",
+        name: "startQ",
+        message: "What would you like to do?",
+        choices: [
+          "view all employees",
+          "view all roles",
+          "view all departments",
+          "add employee",
+          "add department",
+          "add role",
+          "update employee role",
+          "remove employee"
+        ]
+      })
+      .then(function(answer) {
+        console.log(answer);
+        // start of switch statment for user choice
+        switch (answer.startQ) {
+          case "view all employees":
+            viewallemployees();
+            break;
+  
+          case "view all roles":
+            viewallroles();
+            break;
+  
+          case "view all departments":
+            viewalldepartments();
+            break;
+  
+          case "add employee":
+            addEmployee();
+            break;
+  
+          case "update employee role":
+            updateEmpRole();
+            break;
+  
+          case "add department":
+            addDepartment();
+            break;
+  
+          case "add role":
+            addRole();
+            break;
+        }
+      });
+  };
+
+  askQ();
+
+  function viewalldepartments() {
+    connection.query("SELECT * FROM department", function(err, answer) {
+      console.log("\n Departments Retrieved from Database \n");
+      console.table(answer);
+    });
+    askQ();
+  }
+
+  function viewallroles() {
+    connection.query("SELECT * FROM role", function(err, answer) {
+      console.log("\n Roles Retrieved from Database \n");
+      console.table(answer);
+    });
+    askQ();
+  }
+
+  function viewallemployees() {
+    console.log("retrieving employess from database");
+    var fancyQuery =
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department on role.department_id = department.id;";
+    connection.query(fancyQuery, function(err, answer) {
+      console.log("\n Employees retrieved from Database \n");
+      console.table(answer);
+    });
+    askQ();
+  }
+
+  function addDepartment() {
+    inquirer
+      .prompt({
+        type: "input",
+        message: "enter department name",
+        name: "dept"
+      })
+      .then(function(answer) {
+        connection.query(
+          "INSERT INTO department SET ?",
+          {
+            name: answer.dept
+          },
+          function(err, answer) {
+            if (err) {
+              throw err;
+            }
+          }
+        ),
+          console.table(answer);
+        askQ();
+      });
+  }
+
+
+  
+
+
+  
 
 
